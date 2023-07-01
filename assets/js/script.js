d0f7489d55b084492bc87ce1d686f41ea70fb9ee
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-07-01 15:42:20 -0400

    add more info buttons on card divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,8 @@ let historyCard = document.querySelector('#historyCard');
 let infoCard = document.querySelector('#plantInfo');
 let searchBtn = document.getElementById('search-btn');
 let plantsContainer = document.getElementById('plants-container');
-//this may change if cards are created for each plant searched
+
+document.getElementById('plantInfo').hidden = true
 
 searchBtn.addEventListener('click', function() {
     let searchText = searchInput.value.trim()
@@ -49,7 +50,12 @@ function showPlants() {
   
             <div class="content">
             <p class="title is-5">${plant.scientific_name[0]}</p>
-            
+
+      
+            <button class="js-modal-trigger button is-success is-rounded" data-target="modal-js-example">
+              More Information
+            </button>
+     
             </div>
           </div>
         </div>
@@ -61,55 +67,6 @@ function showPlants() {
         return;
 
      }
-     
-    //  if (searchInput = "")
-    //  infoCard.getAttribute.
 
     plantsContainer.innerHTML=html
 }
-//this might not be used this way if bulma code works better///
-// plantInfo.addEventListener('click', function() {
-//     let chosenPlant = "${plant.common_name}"
-//     let url = `${plantApiRootUrl}/species-care-guide-list?page=1&key=${plantApiKey}&q=${plants.common_name}`
-
-// })
-
-//this is from class miniproject - attempting to modify for this application--might not use///
-// var showPlants = function (data, searchTerm) {
-//     if (data.length === 0) {
-//       repoContainerEl.textContent = 'No repositories found.';
-//       // Without a `return` statement, the rest of this function will continue to run and perhaps throw an error if `repos` is empty
-//       return;
-//     }
-  
-//     infoCard.textContent = searchTerm;
-  
-//     for (var i = 0; i < data.length; i++) {
-     
-//       var repoName = repos[i].owner.login + '/' + repos[i].name;
-  
-//       var repoEl = document.createElement('div');
-//       repoEl.classList = 'list-item flex-row justify-space-between align-center';
-  
-//       var titleEl = document.createElement('span');
-//       titleEl.textContent = repoName;
-  
-//       repoEl.appendChild(titleEl);
-  
-//       var statusEl = document.createElement('span');
-//       statusEl.classList = 'flex-row align-center';
-  
-//       if (repos[i].open_issues_count > 0) {
-//         statusEl.innerHTML =
-//           "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-//       } else {
-//         statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-//       }
-  
-//       repoEl.appendChild(statusEl);
-  
-//       repoContainerEl.appendChild(repoEl);
-//     }
-//   };
-
-  ////////////////////////////
