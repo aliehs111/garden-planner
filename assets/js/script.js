bdd48c32829bc85bb764896b91fbcff43a5623d9
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-07-06 21:17:13 -0400

    style changes to display plage and change placeholder image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 let plants = [];
 let plantApiRootUrl = 'https://perenual.com/api';
 let plantApiKey = 'sk-tsf9649c6f20e14401425';
-let placeholderImg = 'https://img.freepik.com/free-vector/plant-emoji_78370-262.jpg?w=1060&t=st=1688479335~exp=1688479935~hmac=dc478ee6f428074956158d7e9224b09456b51e2872f04ff3c69bf4256d1de9e9'
+let placeholderImg = "./assets/images/leaf_placeholder.jpg"
 
 //DOM Elements
 let searchInput = document.querySelector('#search-input');
@@ -33,7 +33,7 @@ function showPlants() {
   plantsContainer.innerHTML = '';
   let html = "";
 
-  for (let index in plants) {    
+  for (let index in plants) {
     let plant = plants[index]
     if (plant.common_name.toLowerCase().includes(searchInput.value.trim().toLowerCase())) {
       html += `
@@ -54,7 +54,7 @@ function showPlants() {
           <p class="title is-5">${plant.scientific_name[0]}</p>
 
     
-          <button id="${index}" class="open_modal button is-success is-rounded" data-target="plant-modal">
+          <button id="${index}" class="open_modal button is-rounded" data-target="plant-modal">
             More Information
           </button>
           <button class="local_storage button is-warning is-rounded">Add to Garden</button>
@@ -64,16 +64,13 @@ function showPlants() {
     </div>
    <button id="close-modal" class="modal-close is-large" aria-label="close"></button>`
     }
-    
+
 
   }
   if (plants.length === 0) {
     plantsContainer.textContent = "No plants found";
     return;
   }
-
-
-
 
   plantsContainer.innerHTML = html
 
@@ -82,18 +79,13 @@ function showPlants() {
 
   openModalBtns.forEach(function (el) {
     el.addEventListener('click', function (event) {
-      // modal.classList.add('is-active');
-      // console.log(event.target.id);
       let index = event.target.id;
       let selectedPlant = plants[index]
       console.log(selectedPlant);
-      // JSON.stringify(selectedPlant)
       showDescription(selectedPlant);
     });
 
   })
-
-
 }
 
 function showDescription(plant) {
@@ -104,7 +96,7 @@ function showDescription(plant) {
           <div class="modal-background"></div>
           <div class="modal-card has background-white py-5 px-5">
             <header class="modal-card-head">
-               <p class="modal-card-title">Plant Name: ${plant.common_name}</p>
+               <p class="modal-card-title">${plant.common_name}</p>
               <button class="delete" aria-label="close" onclick="closeModal()"></button>
             </header>
             <figure class="image is-4by3">
@@ -115,7 +107,7 @@ function showDescription(plant) {
               <p>Sunlight requirements: ${plant.sunlight}</p>
             </section>
             <footer class="modal-card-foot">
-              <button class="button is-success">Add to Garden</button>
+            
               <button class="button close-modal" onclick="closeModal()">Close</button>
             </footer>
            
@@ -132,3 +124,8 @@ function closeModal() {
 
 let modal = document.querySelector(".modal");
 let modClsBtns = document.querySelector(".close-modal");
+
+
+
+
+
