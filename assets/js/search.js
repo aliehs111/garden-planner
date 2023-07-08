f3d083362979554563c21ae79e19928e20640c33
Jamie Branch <schwung988@yahoo.com>
2023-07-08 17:48:03 -0400

    adding featureapi branch

---
+++ assets/js/search.js
@@ -0,0 +1,29 @@
+var apiUrl = 'https://api.geoapify.com/v2/places?categories=commercial.garden&filter=place:519a9428736f5e53c05927ae57eec7c34240f00101f90188f83a0000000000c00209920308526963686d6f6e64&limit=20&apiKey=9cf555b9f2b34c039bea2f4d4a04b3cf';
+var ul = document.createElement('ul');
+var data = {
+  name: 'Store'
+}
+
+var fetchData = {
+  method: 'GET',
+  mode: 'no-cors',
+  headers: new Headers({
+    'Content-Type': 'application/json; charset=UTF-8'
+  })
+}
+
+fetch('https://api.geoapify.com/v2/places?categories=commercial.garden&filter=place:519a9428736f5e53c05927ae57eec7c34240f00101f90188f83a0000000000c00209920308526963686d6f6e64&limit=20&apiKey=9cf555b9f2b34c039bea2f4d4a04b3cf')
+  .then((response) => {
+    //handle the response
+    return response.json();
+  })
+  .then((data) => {
+    var stores = data;
+    console.log(stores);
+  })
+  .catch(function(error){
+    console.log(error);
+  });
+
+ // ul.appendChild(list);
+
